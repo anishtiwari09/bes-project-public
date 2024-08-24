41ed2b328e2166b6a87c68bd0339302f04415ad0
Anish Tewari <aneeshtiwari24@gmail.com>
2024-08-25 01:36:23 +0530

    hotfix: dupliccate mobile issue

--- app/backend/models/user_member.model.js
+++ app/backend/models/user_member.model.js
@@ -27,7 +27,7 @@ const userMember = new mongoose.Schema(
     },
     mobile: {
       type: String,
-
+      sparse: true,
       unique: [true, "This mobile number is already exist"],
     },
     email: {
