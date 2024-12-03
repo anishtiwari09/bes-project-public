a9cc5ddeafccaa896af4d6b6ea6c805f38ed611d
Anish Tewari <aneeshtiwari24@gmail.com>
2024-12-03 16:42:43 +0530

    change

--- app/event_conference/bes_expo/exibition/sponsorship_opportunities/page.jsx
+++ app/event_conference/bes_expo/exibition/sponsorship_opportunities/page.jsx
@@ -1,5 +1,20 @@
 import React from "react";
 
 export default function page() {
-  return <div>Sponsorship Opportunities</div>;
+  return (
+    <div
+      className={"flex flex-col max-w-[800px] p-4 " + styles.content_container}
+    >
+      <h2 className="text-[26px] font-bold">Sponsorship Opportunities</h2>
+
+      <div>
+        <p>
+        Sponsorship opportunities at the expo are generally in great demand. Exhibitors/Sponsorer desirous of sponsoring any of the above activities/item(s) are requested to contact Broadcast Engineering Society (India) immediately on bes@besindia.com. 
+
+
+
+        </p>
+      </div>
+    </div>
+  );<div>Sponsorship Opportunities</div>;
 }
