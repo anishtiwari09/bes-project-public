f33c3a6e70fa95fd9a8d585cdcce61184f3d107c
Anish Tewari <aneeshtiwari24@gmail.com>
2024-12-21 00:18:06 +0530

    remvoe border bottom

--- app/event_conference/bes_expo/exibition/sponsorship_opportunities/page.jsx
+++ app/event_conference/bes_expo/exibition/sponsorship_opportunities/page.jsx
@@ -17,7 +17,7 @@ export default function page() {
         </p>
 
       </div>
-      <a href="/pdf/others/sponsorship_opportunity.pdf" target="_blank" className='text-blue-900 hover:underline hover:border-b-0'>Download Sponsorship opportunities for BES Expo 2025</a>
+      <a href="/pdf/others/sponsorship_opportunity.pdf" target="_blank" className='text-blue-900 hover:underline hover:border-b-0' style={{borderBottom:0}}>Download Sponsorship opportunities for BES Expo 2025</a>
     </div>
   );
 }
