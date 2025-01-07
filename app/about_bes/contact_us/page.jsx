ea53ea120d3db265bf72e9c86a74ac455a2c87aa
Anish Tewari <aneeshtiwari24@gmail.com>
2025-01-08 02:02:44 +0530

    remove spade

--- app/about_bes/contact_us/page.jsx
+++ app/about_bes/contact_us/page.jsx
@@ -17,7 +17,7 @@ export default function page() {
             New Delhi-110001 Tel: +91 11 23316709
           </span>
           <span className="pl-0 block">
-           Email: &nbps;<a href='mailto:bes@besindia.com'>bes@besindia.com</a>
+           Email: &nbsp;<a href='mailto:bes@besindia.com'>bes@besindia.com</a>
           </span>
         </p>
       </div>
