e42f66271298feb4e6d5d595bed09010d53afb4e
Anish Tewari <aneeshtiwari24@gmail.com>
2025-01-22 17:35:19 +0530

    remove date

--- app/UIComponent/Carousel/HomePage/Notification.jsx
+++ app/UIComponent/Carousel/HomePage/Notification.jsx
@@ -16,13 +16,13 @@ export default function Notification() {
           BES EXPO-2025: {HOMEPAGE.eventCount} International Conference &
           Exhibition on Broadcast & Media Technology
         </h4>
-        <h3 className="font-bold text-2xl text-[#faac1d] text-center conference_text conference_text2 text-shadow">
+        {/* <h3 className="font-bold text-2xl text-[#faac1d] text-center conference_text conference_text2 text-shadow">
           {HOMEPAGE.expoStartDate.date}
           {HOMEPAGE.expoStartDate.postFix} {HOMEPAGE.expoStartDate.displayMonth}{" "}
           to {HOMEPAGE.expoEndDate.date}
           {HOMEPAGE.expoEndDate.postFix} {HOMEPAGE.expoEndDate.displayMonth}{" "}
           {HOMEPAGE.expoEndDate.year}
-        </h3>
+        </h3> */}
       </React.Fragment>
 
       <h3 className="font-bold text-md text-white bg-[#ff008499] w-fit p-3 text-center conference_text">
