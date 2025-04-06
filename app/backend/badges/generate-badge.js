126d0f5280a679182095ece3bd45d1d79944f619
Anish Tewari <aneeshtiwari24@gmail.com>
2025-04-06 21:14:19 +0530

    generate badge

--- app/backend/badges/generate-badge.js
+++ app/backend/badges/generate-badge.js
@@ -1,23 +1,18 @@
+import chromium from '@sparticuz/chromium';
 import puppeteer from 'puppeteer-core';
-import chromium from 'chrome-aws-lambda';
-import { badgetemplage } from './badge-templage.js'; // make sure this is server-only too
 
-export const generateBadgePdf = async (name, company, qrCodeUrl, urn) => {
-  const executablePath = await chromium.executablePath || '/usr/bin/google-chrome';
-
+export const generateBadgePdf = async (html) => {
   const browser = await puppeteer.launch({
     args: chromium.args,
-    defaultViewport: chromium.defaultViewport,
-    executablePath,
+    executablePath: await chromium.executablePath,
     headless: chromium.headless,
   });
 
   const page = await browser.newPage();
-  await page.setContent(badgetemplage(name, company, qrCodeUrl, urn), {
-    waitUntil: 'networkidle0',
-  });
+  await page.setContent(html, { waitUntil: 'networkidle0' });
 
   const pdfBuffer = await page.pdf({ format: 'A4', printBackground: true });
+
   await browser.close();
   return pdfBuffer;
 };
