724f9cb8b55492b6bf78c5f80746bec1dfe3a572
Anish Tewari <aneeshtiwari24@gmail.com>
2025-04-06 21:56:16 +0530

    fix vercel issue

--- app/backend/badges/generate-badge.js
+++ app/backend/badges/generate-badge.js
@@ -1,23 +1,21 @@
 import puppeteer from "puppeteer-core";
-import chromium from "@sparticuz/chromium"; // handles Vercel-compatible Chromium
+import chromium from "@sparticuz/chromium";
 import { badgetemplage } from "./badge-templage.js";
 
 export const generateBadgePdf = async (name, company, qrCodeUrl, urn) => {
-  //test
-  const isVercel = !!process.env.AWS_REGION; // Vercel sets AWS_REGION in serverless environment
+  const html = badgetemplage(name, company, qrCodeUrl, urn);
 
-  const executablePath = isVercel
-    ? await chromium.executablePath
-    : "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"; // Local Chrome path (Mac)
-
+  const isVercel = !!process.env.VERCEL;
+console.log({isVercel})
   const browser = await puppeteer.launch({
     args: isVercel ? chromium.args : [],
-    executablePath,
-    headless: chromium.headless, // chromium.headless works well for both
+    executablePath: isVercel
+      ? await chromium.executablePath
+      : "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+    headless: true,
   });
 
   const page = await browser.newPage();
-  const html = badgetemplage(name, company, qrCodeUrl, urn);
   await page.setContent(html, { waitUntil: "networkidle0" });
 
   const pdfBuffer = await page.pdf({ format: "A4", printBackground: true });
