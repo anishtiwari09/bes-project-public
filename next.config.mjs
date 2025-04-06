66484b3481555a10596c918e39147d0ed572377f
Anish Tewari <aneeshtiwari24@gmail.com>
2025-04-06 20:08:24 +0530

    build failed issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     eslint: {
-        ignoreDuringBuilds: true,
+      ignoreDuringBuilds: true,
     },
-};
-
-export default nextConfig;
+    webpack(config, { isServer }) {
+      config.module.rules.push({
+        test: /\.js\.map$/,
+        use: 'ignore-loader',
+      });
+      return config;
+    },
+  };
+  
+  export default nextConfig;
+  
