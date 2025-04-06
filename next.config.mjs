87c0c34ade966c6d0f06834365ad614b4c29f44e
Anish Tewari <aneeshtiwari24@gmail.com>
2025-04-06 23:08:43 +0530

    update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    eslint: {
-      ignoreDuringBuilds: true,
-    },
-    webpack(config, { isServer }) {
-      config.module.rules.push({
-        test: /\.js\.map$/,
-        use: 'ignore-loader',
-      });
-      return config;
-    },
-  };
-  
-  export default nextConfig;
-  
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  webpack(config, { isServer }) {
+    config.module.rules.push({
+      test: /\.js\.map$/,
+      use: 'ignore-loader',
+    });
+    return config;
+  },
+  experimental: {
+    // 👇 Add this part to make puppeteer-core & chromium work in serverless (Vercel)
+    serverComponentsExternalPackages: ["puppeteer-core", "@sparticuz/chromium"],
+  },
+};
+
+export default nextConfig;
