a99e308b5e9dfb5f9ac86b3346605312801050f8
atewari <atewari@dxlg.com>
2025-09-28 21:50:01 +0530

    added brocher link

---
+++ app/UIComponent/buttons/download-brochure.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { Button } from "@mui/material";
+import DownloadIcon from "@mui/icons-material/Download";
+import Link from "next/link";
+import { DYNAMIC_LINK } from "@/app/general/commonPath";
+
+export default function DownloadBrochureButton() {
+  return (
+    <Link
+      href={DYNAMIC_LINK.brochure}
+      //   download="brochure.pdf"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="fixed z-40 right-6 bottom-10"
+    >
+      <Button
+        variant="contained"
+        className="flex flex-col items-center bg-[#222fda] text-[20px] font-[500] hover:bg-[#ffffff] hover:text-[#000] shadow-lg p-2"
+        style={{ minWidth: 40, maxWidth: 40 }}
+      >
+        {/* Icon on top */}
+
+        {/* Vertical Text */}
+        <span className="mt-1 flex flex-col leading-4">
+          {"BROCHURE | 2026".split("").map((letter, i) => (
+            <span key={i}>{letter}</span>
+          ))}
+        </span>
+        <DownloadIcon fontSize="medium" />
+      </Button>
+    </Link>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,13 @@ import { getSliderImages } from "./Utility/lib/file";
 import NotificationText from "./UIComponent/Carousel/HomePage/NotificationText";
 import ExclusiveGallery from "./UIComponent/Carousel/HomePage/ExclusiveGallery";
 import MarginTopSpace from "./UIComponent/Carousel/HomePage/MarginTopSpace";
+import DownloadBrochureButton from "./UIComponent/buttons/download-brochure";
 export default async function Home() {
   let data =
     getSliderImages(HOMEPAGE.sliderImageDir + HOMEPAGE.currentYear) || [];
   return (
     <div className="relative">
+      <DownloadBrochureButton />
       <div className="relative">
         <div className="flex gap-2 flex-wrap py-[10px] px-[10px] pb-8 bg-[#eae99b69]">
           <div className="max-w-[500px] mobile_notification_css">
