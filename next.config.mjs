e84a38734c035df4e58accbacbb5163bacfdd8c9
atewari <atewari@dxlg.com>
2025-10-28 13:51:26 +0530

    added analyzer

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
+import bundleAnalyzer from "@next/bundle-analyzer";
+
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
+
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
@@ -46,4 +52,4 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+export default withBundleAnalyzer(nextConfig);
