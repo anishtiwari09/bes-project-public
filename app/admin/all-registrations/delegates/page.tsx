f6c570a613099d2b790bdcb8847c35479628c4da
atewari <atewari@dxlg.com>
2025-12-15 16:52:22 +0530

    fix build error

--- app/admin/all-registrations/delegates/page.tsx
+++ app/admin/all-registrations/delegates/page.tsx
@@ -1,9 +1,19 @@
 import AdminServices from "@/app/backend/lib/services/admin-services";
 import React from "react";
 import AllRegistrations from "./ui/all-registrations";
-
-export default async function page() {
+async function getCookieData() {
   const admin = new AdminServices();
-  const db = await admin.getAllDelegateUsers();
+  return new Promise(async (resolve) => {
+    try {
+      const db = await admin.getAllDelegateUsers();
+      resolve(db);
+    } catch (e) {
+      console.log("error while fetching cookies", e?.message);
+      resolve(null);
+    }
+  });
+}
+export default async function page() {
+  const db = await getCookieData();
   return <AllRegistrations db={(db || []) as any} />;
 }
