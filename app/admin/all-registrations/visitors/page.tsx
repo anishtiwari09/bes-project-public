76c58afd6a8a953ed85246e76907080a942ac8f6
atewari <atewari@dxlg.com>
2025-12-15 17:08:20 +0530

    fix build failed

--- app/admin/all-registrations/visitors/page.tsx
+++ app/admin/all-registrations/visitors/page.tsx
@@ -1,7 +1,7 @@
 import AdminServices from "@/app/backend/lib/services/admin-services";
 import React from "react";
 import AllVisitors from "./ui/all-visitors";
-
+export const dynamic = "force-dynamic";
 export default async function page() {
   const adminServices = new AdminServices();
   const db = await adminServices.getAllVisitorDetails();
