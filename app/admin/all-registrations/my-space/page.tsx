e2d44726de4c109063734b7cf67a7310279e402b
atewari <atewari@dxlg.com>
2025-12-23 20:14:05 +0530

    added cookie

--- app/admin/all-registrations/my-space/page.tsx
+++ app/admin/all-registrations/my-space/page.tsx
@@ -1,7 +1,7 @@
 import AdminServices from "@/app/backend/lib/services/admin-services";
 import React from "react";
 import AllRegistrations from "./ui/all-registrations";
-
+export const dynamic = "force-dynamic";
 export default async function page() {
   const adminServices = new AdminServices(true);
   const db = await adminServices.getMySpaceRegistrations();
