8c172908f7845868158e70845458bcd30e9bd9f4
desktop-computer <shantiwari44@yahoo.com>
2026-02-01 19:23:45 +0530

    change

--- app/Utility/Constant.ts
+++ app/Utility/Constant.ts
@@ -24,21 +24,7 @@ export const OTHERPAGE = {
 
 export const NOTIFICATION_TEXT = {
   contents: [
-    {
-      text: "Invitation for BES Expo 2026",
-      href: "/pdf/others/invitation-for-bes-expo-2026.pdf",
-      target: "_blank",
-    },
-    {
-      text: "Delegate Registration form BES Expo-2026",
-      href: "/pdf/others/registration-form-2026.pdf",
-      target: "_blank",
-    },
-    {
-      text: "Opportunity for Startups in BES EXPO 2026",
-      href: "/pdf/others/startup-bes2026.pdf",
-      target: "_blank",
-    },
+  
   ],
   isShow: true,
 };
